7e586322d4c57ec9dd86a398b63b3d8f53d6cc6b
Sigmu92 <maxwellw92@gmail.com>
2021-03-13 16:31:16 -0500

    attempting to add Tabsy

--- src/pageLoad.js
+++ src/pageLoad.js
@@ -1,6 +1,20 @@
 function pageLoad() {
-  const content = document.querySelector('#content')
+
+  const content = document.querySelector('#mainContent')
+
   const logo = document.createElement('img')
   logo.src = "Restaurant_Logo.png"
-  content.add(logo)
+  content.appendChild(logo)
+
+  const title = document.createElement("div")
+  title.textContent = "Grill Kings Rules!"
+  title.classList.add("headline");
+  content.appendChild(title);
+
+  const text = document.createElement("div")
+  text.textContent = "Made up text about the restaurant goes here!"
+  text.classList.add("descript");
+  content.appendChild(text);
 }
+
+export default pageLoad()
