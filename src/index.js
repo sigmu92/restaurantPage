959b62e13d93deab6ac3a84a30f8466e6619108e
Sigmu92 <maxwellw92@gmail.com>
2021-03-13 16:08:19 -0500

    adding javascript functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-console.log("Test")
+import pageLoad from './pageLoad'
+
+pageLoad()
---
+++ src/pageLoad.js
@@ -0,0 +1,6 @@
+function pageLoad() {
+  const content = document.querySelector('#content')
+  const logo = document.createElement('img')
+  logo.src = "Restaurant_Logo.png"
+  content.add(logo)
+}
